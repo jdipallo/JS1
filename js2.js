569cc40c891c35cc1069da6e078f857ddf1a7c41
jdipallo <jdipallo@gmail.com>
2016-05-20 04:39:30 +0000

    Part I of JS1...

---
+++ js2.js
@@ -0,0 +1,5 @@
+// script to prompt user for their name, then respond with "Hello..."
+// this is done with a prompt then alert popups
+
+var name = prompt("What is your name please? ", "name");
+alert("Hello " + name);
